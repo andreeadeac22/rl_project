c8794d8f400d253e58e42fd703cf1c14b3547844
Andreea <andreeadeac22@gmail.com>
2020-05-10 04:43:25 +0100

    arch script

---
+++ arch_script.sh
@@ -0,0 +1,3 @@
+python train.py --test_graph_type='erdos' --message_function='mpnn' --message_function=1 --neighbour_state_aggr='sum' --hidden_dim=32 && 
+python train.py --test_graph_type='erdos' --message_function='mpnn' --message_function=2 --neighbour_state_aggr='sum' --hidden_dim=16 && 
+python train.py --test_graph_type='erdos' --message_function='mpnn' --message_function=1 --neighbour_state_aggr='mean' --hidden_dim=32
